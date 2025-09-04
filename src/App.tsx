2a8a5057f63940259b98576aff39b8d095b5a3d1
Jatin Sharma <j9599231642@gmail.com>
2025-09-05 00:01:56 +0530

    feat. Alert , Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
-  return <div><ListGroup /></div>
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+
+    <div>
+      { alertVisible && <Alert onClose={ () => setAlertVisibility(false)}>My Alert</Alert>}
+      <Button color="primary" onClick={ () => setAlertVisibility(true)}>My Button</Button>
+    </div>
+  );
 }
 
 export default App;
